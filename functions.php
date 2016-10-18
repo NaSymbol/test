ecc1f32722522e739a3c9774ea5d57e3507a369f
symbol <symbol_for_success@hotmail.com>
2016-10-19 09:26:10 +1300

    starting point

--- functions.php
+++ functions.php
@@ -103,9 +103,12 @@ add_action( 'widgets_init', 'test_widgets_init' );
  */
 function test_scripts() {
 	wp_enqueue_style( 'test-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
 
 	wp_enqueue_script( 'test-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), )
+	wp_enqueue_script( 'jquery','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), true);
+	wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"', array(), true );
+
 
 	wp_enqueue_script( 'test-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'test' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
+	<header id="masthead" class="site-header" role="banner" data-spy="affix" data-offset-top="60" data-offset-bottom="200">
 		<div class="site-branding">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
@@ -46,4 +46,4 @@
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+	<div id="content" class="site-content container">
